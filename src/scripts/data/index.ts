ef31c283aaa0a801c3faf32a8e874074b800a50c
Oden <ops991923@gmail.com>
2017-05-02 13:34:54 -0700

    Add router state

--- src/scripts/data/index.ts
+++ src/scripts/data/index.ts
@@ -1,9 +1,11 @@
+import {RouterState} from "connected-react-router";
 import {combineReducers} from "redux";
 
 import counter, * as counterAll from "./counter";
 
 export interface State {
 	counter: counterAll.State;
+	router: RouterState;
 }
 
 export interface TypedAction<T extends string> {
