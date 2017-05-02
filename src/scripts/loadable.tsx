57522891416995d53aa6c39d43aa6f1b5cf69337
Oden <ops991923@gmail.com>
2017-05-02 13:34:32 -0700

    remove unused null assertion

--- src/scripts/loadable.tsx
+++ src/scripts/loadable.tsx
@@ -1,8 +1,8 @@
 import Loadable from "react-loadable";
 
 export default function(loader: () => Promise<any>) {
-	return Loadable<any>({
+	return Loadable<any, any>({
 		loader,
-		LoadingComponent: () => null!
+		LoadingComponent: () => null
 	});
 }
