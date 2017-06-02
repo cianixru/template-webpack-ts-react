962ae3e6d936f9f0ab6ed85e1699230405ec65ee
Oden <ops991923@gmail.com>
2017-06-02 13:09:55 -0700

    Add async checkbox to Counter to test redux-observable.

--- src/scripts/pages/counter/index.tsx
+++ src/scripts/pages/counter/index.tsx
@@ -4,44 +4,62 @@ import {connect} from "react-redux";
 import {translate, InjectedTranslateProps} from "react-i18next";
 import {Action, Dispatch} from "redux";
 import {State} from "data";
-import {increase, decrease, set} from "data/counter";
+import {increase, decrease, increaseAsync, decreaseAsync, set} from "data/counter";
 
 import style from "./style.scss";
 
 @translate("counter")
 class Counter extends React.Component<Props, OwnState> {
 	state = {
-		input: 0
+		async: false
 	};
 
-	onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	onAsyncChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({
+			async: e.target.checked
+		});
+	}
+
+	onNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const value = e.target.valueAsNumber;
 		this.props.set(Number.isFinite(value) ? value : this.props.value);
 	}
 
 	render() {
 		const t = this.props.t!;
+		const increaseFunc = this.state.async ? this.props.increaseAsync : this.props.increase;
+		const decreaseFunc = this.state.async ? this.props.increaseAsync : this.props.increase;
 
 		return (
 			<section className={style.container}>
 				<Helmet title={t("title")} />
 
 				<h2>{this.props.value}</h2>
+
+				<div>
+					<span>Async </span>
+					<input
+						type="checkbox"
+						checked={this.state.async}
+						onChange={this.onAsyncChange}
+					/>
+				</div>
+
 				<div className={style.controls}>
-					<button onClick={this.props.decrease.bind(this, 1)}>-</button>
+					<button onClick={decreaseFunc.bind(null, 1)}>-</button>
 					<input
 						type="number"
 						value={this.props.value}
-						onChange={this.onChange}
+						onChange={this.onNumberChange}
 					/>
-					<button onClick={this.props.increase.bind(this, 1)}>+</button>
+					<button onClick={increaseFunc.bind(null, 1)}>+</button>
 				</div>
 			</section>
 		);
 	}
 }
 
-type Props = StateProps & DispatchProps & OwnProps & InjectedTranslateProps;
+type Props = StateProps & DispatchProps & InjectedTranslateProps;
 
 interface StateProps {
 	value: number;
@@ -50,15 +68,13 @@ interface StateProps {
 interface DispatchProps {
 	increase(value: number): void;
 	decrease(value: number): void;
+	increaseAsync(value: number): void;
+	decreaseAsync(value: number): void;
 	set(value: number): void;
 }
 
-interface OwnProps {
-
-}
-
 interface OwnState {
-	input: number;
+	async: boolean;
 }
 
 const mapStateToProps = (state: State) => ({
@@ -68,7 +84,9 @@ const mapStateToProps = (state: State) => ({
 const mapDispatchToProps = (dispatch: Dispatch<Action>) => ({
 	increase: (value: number) => dispatch(increase(value)),
 	decrease: (value: number) => dispatch(decrease(value)),
+	increaseAsync: (value: number) => dispatch(increaseAsync(value)),
+	decreaseAsync: (value: number) => dispatch(decreaseAsync(value)),
 	set: (value: number) => dispatch(set(value))
 });
 
-export default connect<StateProps, DispatchProps, OwnProps>(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect<StateProps, DispatchProps, {}>(mapStateToProps, mapDispatchToProps)(Counter);
