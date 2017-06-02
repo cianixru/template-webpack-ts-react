d7a4e9652b7dd4e3c7df175d3f98342d45fabd14
Oden <ops991923@gmail.com>
2017-06-02 13:09:33 -0700

    Refactor naming, add example epic

--- src/scripts/data/counter/index.ts
+++ src/scripts/data/counter/index.ts
@@ -1,11 +1,14 @@
-import {ActionTypes, Action} from "./actions";
+import {ActionsObservable} from "redux-observable";
+import "rxjs/add/operator/delay";
+import "rxjs/add/operator/map";
+import {Action, ActionTypes, increase, decrease} from "./actions";
 import {State} from "./model";
 
 const initialState: State = {
 	value: 0
 };
 
-export default function(state = initialState, action: Action): State {
+export const reducer = (state = initialState, action: Action): State => {
 	switch (action.type) {
 		case ActionTypes.INCREASE: {
 			return {value: state.value + action.payload.value};
@@ -21,7 +24,24 @@ export default function(state = initialState, action: Action): State {
 
 		default: return state;
 	}
-}
+};
+
+export const epic = (actions$: ActionsObservable<Action>) => actions$
+	.ofType(ActionTypes.INCREASE_ASYNC, ActionTypes.DECREASE_ASYNC)
+	.delay(1000)
+	.map(action => {
+		switch (action.type) {
+			case ActionTypes.INCREASE_ASYNC: {
+				return increase(action.payload.value);
+			}
+
+			case ActionTypes.DECREASE_ASYNC: {
+				return decrease(action.payload.value);
+			}
+
+			default: return undefined;
+		}
+	});
 
 export * from "./actions";
 export * from "./model";
--- src/scripts/data/index.ts
+++ src/scripts/data/index.ts
@@ -1,18 +1,23 @@
-import {RouterState} from "connected-react-router";
 import {combineReducers} from "redux";
+import {combineEpics} from "redux-observable";
+import {RouterState} from "connected-react-router";
 
-import counter, {State as CounterState} from "./counter";
+import * as counter from "./counter";
 
 export interface State {
-	counter: CounterState;
+	counter: counter.State;
 	router: RouterState;
 }
-
-export default combineReducers<State>({
-	counter
-});
 
 export interface TypedAction<T extends string> {
 	readonly type: T;
 	payload: {};
 }
+
+export const rootEpic = combineEpics(
+	counter.epic
+);
+
+export default combineReducers<State>({
+	counter: counter.reducer
+});
